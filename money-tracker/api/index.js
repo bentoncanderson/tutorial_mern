309e198c7bd0be15ac248bcb4e70021095802d8a
benton <bentonandersonwps@gmail.com>
2024-01-25 13:36:59 -0600

    finished tutorial

--- money-tracker/api/index.js
+++ money-tracker/api/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+require('dotenv').config();
 const cors = require('cors');
+const Transaction = require('./models/Transaction.js');
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(cors());
@@ -8,8 +11,17 @@ app.get('/api/test', (req, res) => {
     res.json('test ok3')
 });
 
-app.post('/api/transaction', (req, res) =>{
-    res.json(req.body);
+app.post('/api/transaction', async (req, res) =>{
+    await mongoose.connect(process.env.MONGO_URL)
+    const {name, description, datetime, price} = req.body
+    const transaction = await Transaction.create({name, description, datetime, price})
+    res.json(transaction);
 });
+
+app.get('/api/transactions', async (req, res) => {
+    await mongoose.connect(process.env.MONGO_URL)
+    const transactions = await Transaction.find();
+    res.json(transactions);
+})
 
 app.listen(4000);
--- money-tracker/src/App.js
+++ money-tracker/src/App.js
@@ -1,27 +1,56 @@
 import './App.css';
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 function App() {
   const [name, setName] = useState('');
   const [datetime, setDatetime] = useState('');
   const [description, setDescription] = useState('');
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    getTransactions().then(setTransactions)
+  }, []);
+
+  async function getTransactions() {
+    const url = process.env.REACT_APP_API_URL+'/transactions';
+    const response = await fetch(url)
+    return await response.json();
+  }
+
   function addNewTransaction(ev) {
     ev.preventDefault();
     const url = process.env.REACT_APP_API_URL+'/transaction';
-    console.log(url);
+    const price = name.split(' ')[0];
     fetch(url, {
       method: 'POST',
       headers: {'Content-type':'application/json'},
-      body: JSON.stringify({name, description, datetime})
+      body: JSON.stringify({
+        price: price,
+        name: name.substring(price.length + 1), 
+        description, 
+        datetime
+      })
     }).then((response) => {
       response.json().then((json) => {
+        setName('');
+        setDatetime('');
+        setDescription('');
         console.log('result', (json));
       })
     });
   }
+  let balance = 0;
+  for (const transaction of transactions) {
+    balance = balance + transaction.price;
+  }
+
+  balance = balance.toFixed(2);
+  const fraction = balance.split('.')[1];
+  balance = balance.split('.')[0]
+
   return (
     <main>
-      <h1>$400<span>.00</span></h1>
+      <h1>${balance}<span>{fraction}</span></h1>
       <form onSubmit={addNewTransaction}>
         <div className="basic">
           <input type="text" 
@@ -42,36 +71,22 @@ function App() {
         <button type="submit">Add new transaction</button>
       </form>
       <div className="transactions">
-        <div className="transaction">
-          <div className="left">
-            <div className="name">New Samsung TV</div>
-            <div className="description">it was time for new tv</div>
+        {transactions.length > 0 && transactions.map(transaction => (
+          <div className="transaction">
+            <div className="left">
+              <div className="name">{transaction.name}</div>
+              <div className="description">{transaction.description}</div>
+            </div>
+            <div className = "right">
+              console.log(transaction.price)
+              <div className = {"price " + (transaction.price < 0 ? 'red':'green')}>
+                {transaction.price}
+              </div>
+              <div className = "datetime">2022-12-18 15:45</div>
+            </div>
           </div>
-          <div className = "right">
-            <div className = "price red">-$500</div>
-            <div className = "datetime">2022-12-18 15:45</div>
-          </div>
-        </div>
-        <div className="transaction">
-          <div className="left">
-            <div className="name">Gig job new website </div>
-            <div className="description">it was time for new tv</div>
-          </div>
-          <div className = "right">
-            <div className = "price green">+$400</div>
-            <div className = "datetime">2022-12-18 15:45</div>
-          </div>
-        </div>
-        <div className="transaction">
-          <div className="left">
-            <div className="name">iPhone</div>
-            <div className="description">it was time for new tv</div>
-          </div>
-          <div className = "right">
-            <div className = "price red">-$900</div>
-            <div className = "datetime">2022-12-18 15:45</div>
-          </div>
-        </div>
+        ))}
+        
       </div>
     </main>
   );
